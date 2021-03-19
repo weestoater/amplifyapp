c1a2d6e22f5fa30be0c7efe639fff68f653680f6
Ian <ian@weestoater.com>
2021-03-19 07:57:15 +0000

    updated prettier config; added commonApi component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,15 +12,14 @@ export function About() {
 					</div>
 					<div className="card-body">
 						<p>
-							This is a site for me to play with and learn more about{' '}
-							<code>ReactJS</code> and <code>AWS</code>. I use both technologies
-							in my day job and am aiming to get certified as an AWS Developer
-							this year.
+							This is a site for me to play with and learn more about <code>ReactJS</code> and{' '}
+							<code>AWS</code>. I use both technologies in my day job and am aiming to get certified as an AWS
+							Developer this year.
 						</p>
 
 						<p>
-							I also want to use it to trial different charting libraries and
-							implement the fabulous React Testing Library based on{' '}
+							I also want to use it to trial different charting libraries and implement the fabulous React
+							Testing Library based on{' '}
 							<a href="http://www.testingjavascript.com" target="_blank">
 								testing javascript
 							</a>{' '}
--- src/components/Home/WelcomeCard.js
+++ src/components/Home/WelcomeCard.js
@@ -7,10 +7,7 @@ export function Welcome() {
 				</div>
 				<div className="card-body">
 					<p>This is my little play pen for ReactJs and AWS hobby projects.</p>
-					<p>
-						I'm hoping to add some experiments with ReactJs and dynamic charting
-						soon.
-					</p>
+					<p>I'm hoping to add some experiments with ReactJs and dynamic charting soon.</p>
 				</div>
 			</div>
 		</div>
