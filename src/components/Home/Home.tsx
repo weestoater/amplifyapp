0b258cc839fc1907b9c819bcfe66717da1825516
Ian <ian@weestoater.com>
2021-04-06 01:06:58 +0100

    Convert to TSC; add charts panel;

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Welcome } from './WelcomeCard';
+import Charts from '../Charts/Charts';
 
 export function Home() {
 	return (
 		<div className="row">
 			<Welcome />
+			<Charts />
 		</div>
 	);
 }
