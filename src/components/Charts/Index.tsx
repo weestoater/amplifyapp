a81a7a28b6642e3d06c80443b6e31577b76f13bc
Ian <ian@weestoater.com>
2021-04-18 15:47:24 +0100

    Added area chart for MFC matches

--- src/components/Charts/Index.tsx
+++ src/components/Charts/Index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { AreaChart } from './AreaChart';
 import { PieChart } from './PieChart';
 
 export function ChartsHomePage() {
 	return (
-		<>
+		<div className="row">
+			<AreaChart />
 			<PieChart />
-		</>
+		</div>
 	);
 }
